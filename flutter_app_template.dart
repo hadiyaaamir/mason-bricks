76ce72bb29aff171b0b7f28eed728149980bb0b8
Hadiya <hadiyaaamir222@gmail.com>
2023-11-02 22:41:19 +0500

    getting brick from github

--- flutter_app_template.dart
+++ flutter_app_template.dart
@@ -3,8 +3,11 @@ import 'dart:io';
 import 'package:mason/mason.dart';
 
 Future<void> main() async {
-  final brick = Brick.path(
-    '/Users/hadiya/Documents/projects/mason-templates/mason-bricks/bricks/flutter_app_template',
+  final brick = Brick.git(
+    GitPath(
+      "https://github.com/hadiyaaamir/mason-bricks.git",
+      path: 'bricks/flutter_app_template',
+    ),
   );
 
   Map<String, dynamic> variables = <String, dynamic>{
